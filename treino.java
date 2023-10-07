c33e2353678583d47f502b217206f07874f68b62
Leda Trindade <141334677+ledatrindade@users.noreply.github.com>
2023-10-07 17:08:29 -0300

    Create treino.java
    Treinando variáveis em JAVA

---
+++ treino.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a, b, c;
+
+        a = 11;
+        b = 13;
+        c = a + b;
+
+        int sub = a - b;
+        float div = (float) b / a;
+        int mul = a * b;
+
+
+
+
+        System.out.println(c);
+        System.out.println(div);
+        System.out.println(sub);
+        System.out.println(mul);
+    }
+}
